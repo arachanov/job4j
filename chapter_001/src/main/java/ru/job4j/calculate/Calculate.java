1dcef0bda4317441e2ed2f45e339dcc97bff2384
arachanov <salim@kadastr05.ru>
2019-06-23 00:12:34 +0300

    1.3.1 Maven Plugins

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,27 @@
-package PACKAGE_NAME;
-/** 
- * Класс Calculate
- * @autor Arachanov
- * @version 1.0
-*/
+package ru.job4j.calculate;
+
+/**
+ * Calculate.
+ *
+ * @author Arachanov
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
