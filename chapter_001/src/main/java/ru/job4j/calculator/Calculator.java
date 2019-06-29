2de8676020cedba3b1d8bc4b8debb7f6d3b3d825
arachanov <salim@kadastr05.ru>
2019-06-29 19:55:25 +0300

    3.3. Идеальный вес

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -8,10 +8,13 @@ public class Calculator {
     public double subtract(double first, double second) {
         return first - second;
     }
+
     public double div(double first, double second) {
+
         return first / second;
     }
     public double multiple(double first, double second) {
+
         return first * second;
     }
 }
