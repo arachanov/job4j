d3b87f509ef27084abdd6a6dcc6e2a09085acf47
arachanov <salim@kadastr05.ru>
2019-06-30 02:06:01 +0300

    3.4. Растояние между двумя точками

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculator;
 
+/***
+ * Класс Калькулятор для вычисления
+ * @author Salim
+ * @since 30.06.2019
+ * @version 1
+ */
 public class Calculator {
     public double add(double first, double second) {
         return first + second;
