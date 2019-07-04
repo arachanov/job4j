b29355f0215ec50d942ee771b06ddf5c1d74b02a
arachanov <salim@kadastr05.ru>
2019-07-05 00:12:35 +0300

    6.3. no end

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+/**
+ * @author Salim
+ * @since 05072019
+ * @version 001
+ */
+public class Check {
+    public boolean mono(boolean[] data) {
+        boolean result = false;
+//        for () {
+//        }
+        return result;
+    }
+}
