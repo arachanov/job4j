e7b06768a167434f60cad8f4aad3dc0932a80193
arachanov <salim@kadastr05.ru>
2019-07-18 21:53:08 +0300

    Объединить два массива [#124482]

--- chapter_002/src/main/java/tracker/SortArray.java
+++ chapter_002/src/main/java/tracker/SortArray.java
@@ -1,15 +1,28 @@
-package ru.job4j.tracker;
+package ru.job4j.array;
 
 public class SortArray {
-    public int[] sort (int[] array1, int[] array2) {
-        int dlina = array1.length + array2.length;
-        int[] array3  = new int[dlina];
-        array3[0] = array1[0];
+    public int[] merge(int[] left, int[] right) {
+        int dlina = left.length + right.length;
+        int[] array3 = new int[dlina];
+        array3[0] = left[0];
         for (int i = 1; i < dlina; i++) {
-            if (i < array1.length) {
-                array3[i] = array1[i];
-                if (array1[i] > )
+            if (i < left.length) {
+                if (array3[i - 1] > left[i]) {
+                array3[i] = array3[i - 1];
+                array3[i - 1] = left[i];
+                } else {
+                    array3[i] = left[i];
+                }
+
+            } else {
+                if (array3[i - 1] > right[i - right.length]) {
+                    array3[i] = array3[i - 1];
+                    array3[i - 1] = right[i - right.length];
+                } else {
+                    array3[i] = right[i - right.length];
+                }
             }
         }
+        return array3;
     }
 }
--- chapter_002/src/test/java/tracker/SortArrayTest.java
+++ chapter_002/src/test/java/tracker/SortArrayTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.tracker;
+package ru.job4j.array;
 
 import org.junit.Test;
 
@@ -7,13 +7,33 @@ import static org.junit.Assert.assertThat;
 
 public class SortArrayTest {
     @Test
-    public void testMergeArrays() {
+    public void testArrays() {
         SortArray array = new SortArray();
         int[] array1 = new int[]{1, 3};
         int[] array2 = new int[]{2, 4};
-        int[] result = array.sort(array1, array2);
+        int[] result = array.merge(array1, array2);
         int[] expect = new int[]{1, 2, 3, 4};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void testeArrays() {
+        SortArray array = new SortArray();
+        int[] array1 = new int[]{0, 2, 1};
+        int[] array2 = new int[]{3, 5, 4};
+        int[] result = array.merge(array1, array2);
+        int[] expect = new int[]{0, 1, 2, 3, 4, 5};
         assertThat(result, is(expect));
 
     }
+    /*
+    @Test
+    public void testeArrays() {
+        SortArray array = new SortArray();
+        int[] array1 = new int[]{5, 3, 4};
+        int[] array2 = new int[]{0, 2, 1};
+        int[] result = array.merge(array1, array2);
+        int[] expect = new int[]{0, 1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+
+    }*/
 }
