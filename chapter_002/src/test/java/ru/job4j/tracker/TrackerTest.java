0cd69a04b08d53ec2dc7a0bf03ab5fb97f4a5cc9
arachanov <salim@kadastr05.ru>
2019-07-31 00:02:21 +0300

    1. Реализация профессий в коде[#152050]

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,25 +1,32 @@
 package ru.job4j.tracker;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.experimental.categories.Categories;
-import ru.job4j.jobs.*;
-import static org.hamcrest.number.IsCloseTo.closeTo;
-import static org.junit.Assert.*;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
-@Ignore
 public class TrackerTest {
     @Test
-    public void proffesionTest() {
-        Tracker track = new Tracker();
-        String result = track.imya();
-        assertThat(result, is("Salim"));
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        long created = System.currentTimeMillis();
+        Item item = new Item("test1", "testDescription", created);
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
     }
     @Test
-    public void checkDiagnose() {
-        Pacient pacient = new Pacient("Steev", 55);
-        Doctor doc = new Doctor();
-        assertThat(doc.heal(pacient), is(false));
+    public void whenReplaceNameThenReturnNewName() {
+        Tracker tracker = new Tracker();
+        Item previous = new Item("test1", "testDescription", 123L);
+        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+        tracker.add(previous);
+        // Создаем новую заявку.
+        Item next = new Item("test2", "testDescription2", 1234L);
+        // Проставляем старый id из previous, который был сгенерирован выше.
+        next.setId(previous.getId());
+        // Обновляем заявку в трекере.
+        tracker.replace(previous.getId(), next);
+        // Проверяем, что заявка с таким id имеет новые имя test2.
+        assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
     }
 }
