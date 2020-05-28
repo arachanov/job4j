1ace9449d40fc2a0dc78560cede24e697e8f15f0
arachanov <salim@kadastr05.ru>
2020-05-28 17:20:14 +0300

    Сортировка департаментов

---
+++ chapter_004/src/test/java/ru/job4j/lambda/FunctionCounterTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.lambda;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+public class FunctionCounterTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        FunctionCounter function = new FunctionCounter();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenQuadraticFunctionThenLinearResults() {
+        FunctionCounter function = new FunctionCounter();
+        List<Double> result = function.diapason(5, 8, x -> x * x);
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenExponentialFunctionThenLinearResults() {
+        FunctionCounter function = new FunctionCounter();
+        List<Double> result = function.diapason(5, 8, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(32D, 64D, 128D);
+        assertThat(result, is(expected));
+    }
+}
