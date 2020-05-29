65db5afd08926de1076df2b858b8d81a7cf0b3b3
arachanov <salim@kadastr05.ru>
2020-05-30 02:40:35 +0300

    Фильтрация учеников

---
+++ chapter_004/src/main/java/ru/job4j/search/Student.java
@@ -0,0 +1,21 @@
+package ru.job4j.search;
+
+
+public class Student {
+    private int score;
+    private String surname;
+
+    public Student(int score, String surname) {
+        this.score = score;
+        this.surname = surname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+}
---
+++ chapter_004/src/test/java/ru/job4j/search/SchoolTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SchoolTest {
+    @Test
+    public void whenDiaposonA() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(100, "Иванов"));
+        students.add(new Student(60, "Петров"));
+        students.add(new Student(30, "Сидоров"));
+        List<Student> result = School.collect(students, student -> student.getScore() >= 70);
+        assertThat(result.get(0).getSurname(), is("Иванов"));
+
+    }
+    @Test
+    public void whenDiaposonB() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(100, "Иванов"));
+        students.add(new Student(60, "Петров"));
+        students.add(new Student(30, "Сидоров"));
+        List<Student> result = School.collect(students, student -> student.getScore() < 70 & student.getScore() > 30);
+        assertThat(result.get(0).getSurname(), is("Петров"));
+
+    }@Test
+    public void whenDiaposonC() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(100, "Иванов"));
+        students.add(new Student(60, "Петров"));
+        students.add(new Student(30, "Сидоров"));
+        List<Student> result = School.collect(students, student -> student.getScore() <= 30);
+        assertThat(result.get(0).getSurname(), is("Сидоров"));
+
+    }
+}
