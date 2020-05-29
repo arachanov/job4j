cb32cbb7eec0c8ac4f93866bb12dcb28f33721ac
arachanov <salim@kadastr05.ru>
2020-05-30 01:31:51 +0300

    2. В классе StartUI заменить вывод в консоль на Consumer

---
+++ chapter_004/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,32 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /*public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        ...
+        return result;
+    }*/
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> checkAddress = person -> person.getAddress().contains(key);
+        Predicate<Person> checkName = person -> person.getName().contains(key);
+        Predicate<Person> checkSurname = person -> person.getSurname().contains(key);
+        Predicate<Person> checkPhone = person -> person.getPhone().contains(key);
+        Predicate<Person> combine = checkAddress.or(checkName).or(checkSurname).or(checkPhone);
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getName(), is("Petr"));
+    }
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Arsentev");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "5348721", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("5348721");
+        assertThat(persons.get(0).getPhone(), is("5348721"));
+    }
+    @Test
+    public void whenFindByAdress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Bryansk");
+        assertThat(persons.get(0).getAddress(), is("Bryansk"));
+    }
+}
