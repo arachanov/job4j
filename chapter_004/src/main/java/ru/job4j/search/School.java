afd9550fd97b0ef81ec0ce16c8265d503cbe2ee9
arachanov <salim@kadastr05.ru>
2020-05-31 14:25:28 +0300

    4. Преобразование List в Map

--- chapter_004/src/main/java/ru/job4j/search/School.java
+++ chapter_004/src/main/java/ru/job4j/search/School.java
@@ -2,13 +2,14 @@ package ru.job4j.search;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
 
 
-    public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        return students.stream().filter(predict).collect(Collectors.toList());
+    public static Map<String, Student> collect(List<Student> students) {
+        return students.stream().collect(Collectors.toMap(Student::getSurname, student -> student, (firstStudent, secondStudent) -> firstStudent));
     }
 }
--- chapter_004/src/test/java/ru/job4j/search/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/search/SchoolTest.java
@@ -5,36 +5,18 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 public class SchoolTest {
     @Test
-    public void whenDiaposonA() {
+    public void listToMap() {
         ArrayList<Student> students = new ArrayList<>();
         students.add(new Student(100, "Иванов"));
         students.add(new Student(60, "Петров"));
         students.add(new Student(30, "Сидоров"));
-        List<Student> result = School.collect(students, student -> student.getScore() >= 70);
-        assertThat(result.get(0).getSurname(), is("Иванов"));
-
-    }
-    @Test
-    public void whenDiaposonB() {
-        ArrayList<Student> students = new ArrayList<>();
-        students.add(new Student(100, "Иванов"));
-        students.add(new Student(60, "Петров"));
-        students.add(new Student(30, "Сидоров"));
-        List<Student> result = School.collect(students, student -> student.getScore() < 70 & student.getScore() > 50);
-        assertThat(result.get(0).getSurname(), is("Петров"));
-    }
-    @Test
-    public void whenDiaposonC() {
-        ArrayList<Student> students = new ArrayList<>();
-        students.add(new Student(100, "Иванов"));
-        students.add(new Student(60, "Петров"));
-        students.add(new Student(30, "Сидоров"));
-        List<Student> result = School.collect(students, student -> student.getScore() <= 50);
-        assertThat(result.get(0).getSurname(), is("Сидоров"));
-
+        Map<String, Student> result = School.collect(students);
+        assertThat(result.get("Иванов").getScore(), is(100));
+        assertThat(result.get("Петров").getSurname(), is("Петров"));
     }
 }
